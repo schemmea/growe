0717a408f107b35cd7c385807305e65e0f5e3bee
alena <a.schemmert@gmail.com>
2023-09-03 08:30:42 +0200

    correct errors?

--- search_unsuccesful_runs.sh
+++ search_unsuccesful_runs.sh
@@ -7,41 +7,42 @@ path=$1
 
 for base_name in "${base_names[@]}"; do
     for id in {0..6}; do
-        folder="$path/nextflow-${base_name}-${id}"
+       folder="$path/nextflow-${base_name}-${id}"
 
-	errorstring="$folder"
+	      errorstring="$folder"
         if [ -d "$folder" ]; then
             log_file="$folder/executor.log"  # Assuming the log file name is consistent
 
             if [ -f "$log_file" ]; then
                 if ! grep -q "$search_file" "$log_file"; then
-  		    $errorstring="$errostring not $search_file!!!!"
+  	          	    errorstring="$errostring not $search_file!!!!"
                 fi
             else
-                $errorstring="$errostring no $log_file!!!!"
+                errorstring="$errostring no $log_file!!!!"
             fi
 
-           log_file2="$folder/errorDir/cov-all.log"	   
- 	   log_file3="$folder/errorDir/cov-valid.log"
-  	   if  [ -f  "$log_file2" ]; then
-              myfilesize=$(wc -c "$log_file2" | awk '{print $1}')
-		if [[ $myfilesize -eq 0 ]]; then
-		  $errorstring="$log_file2 is empty"
-		fi
-	    else
-               $errorstring="$errostring no $log_file2 !!!!"
-           fi
-	   if  [ -f  "$log_file3" ]; then
-	              myfilesize=$(wc -c "$log_file3" | awk '{print $1}')
-		if [[ $myfilesize -eq 0 ]]; then
-		  $errorstring="$log_file3 is empty"
-		fi
-		else
-              $errorstring="$errostring no $log_file3 !!!!"
+             log_file2="$folder/errorDir/cov-all.log"
+             log_file3="$folder/errorDir/cov-valid.log"
+             if  [ -f  "$log_file2" ]; then
+                    myfilesize=$(wc -c "$log_file2" | awk '{print $1}')
+                if [[ $myfilesize -eq 0 ]]; then
+                  errorstring="cov-all is empty"
+                fi
+            else
+                 errorstring="$errostring no cov-all !!!!"
             fi
-		echo "$errorstring"
+            if  [ -f  "$log_file3" ]; then
+                myfilesize=$(wc -c "$log_file3" | awk '{print $1}')
+                if [[ $myfilesize -eq 0 ]]; then
+                  errorstring="cov-valid is empty"
+                fi
+            else
+                errorstring="$errostring no cov-valid !!!!"
+            fi
+            echo "$errorstring"
+
         else
-		echo "no run for $folder!"
+          echo "no run for $folder!"
         fi
 
     done
