559f602ed12b25cd8ac8c7623d2050ec91bd0584
Alena <alena.sch95@gmail.com>
2023-09-22 21:34:53 +0200

    schriots andpdfs

--- scripts/data-processing/download_data_afl.sh
+++ scripts/data-processing/download_data_afl.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-#remotepath="/home/alena/source/ma_test2/exp1/"
-remotepath="/home/alena/source/growe/expfinalfinal/"
+remotepath="/home/alena/source/ma_test2/exp2/"
 localpath="$HOME/source/repos/growe/expfinalfinal/"
-for prefix in "semantic-zest" "syntactic-zest" "semantic-noguidance" "syntactic-noguidance"; do
-#for prefix in "afl"; do
-    for ((i=32; i<=40; i++)); do
-        #dir="nextflow-${prefix}-${i}/fuzz-results"
-    	dir="nextflow-${prefix}-${i}/errorDir"
+for prefix in "afl"; do
+    for ((i=0; i<=20; i++)); do
+        dir="nextflow-${prefix}-${i}/fuzz-results"
         mkdir -p "$localpath$dir"
         echo "~~Downloading $dir~~"
 
         plot_data="$dir/plot_data"
         cov_all="$dir/cov-all.log"
         cov_valid="$dir/cov-valid.log"
-        exceptions="$dir/exception_log.csv"
+        #exceptions="$dir/exception_log.csv"
+        stats="$dir/fuzzer_stats"
 
         scp alena@213.202.208.247:${remotepath}${plot_data} "${localpath}${plot_data}"
         scp alena@213.202.208.247:${remotepath}${cov_all} "${localpath}${cov_all}"
         scp alena@213.202.208.247:${remotepath}${cov_valid} "${localpath}${cov_valid}"
-        scp alena@213.202.208.247:${remotepath}${exceptions} "${localpath}${exceptions}"
+       # scp alena@213.202.208.247:${remotepath}${exceptions} "${localpath}${exceptions}"
+        scp alena@213.202.208.247:${remotepath}${stats} "${localpath}${stats}"
 
     done
 done
---
+++ scripts/data-processing/download_data_zip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#remotepath="/home/alena/source/ma_test2/exp1/"
+remotepath="/home/alena/source/growe/expfinalfinal.zip"
+localpath="$HOME/"
+
+scp alena@213.202.208.247:${remotepath} "${localpath}/expfinalfinal.zip"
+
+
