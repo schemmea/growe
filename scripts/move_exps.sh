3db6b614041cb643ea71906c41d7f855c7b6793a
Alena <alena.sch95@gmail.com>
2023-09-24 20:15:02 +0200

    resorting data

--- scripts/move_exps.sh
+++ scripts/move_exps.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-for prefix in "semantic-zest" "syntactic-zest" "semantic-noguidance" "syntactic-noguidance"; do
-    for ((i=20; i<=35; i++)); do
-        old_name="../exp2/nextflow-${prefix}-${i}"
-        new_number=$((i+20))
-        new_name="../exp1/nextflow-${prefix}-${i}"
+#for prefix in "semantic-zest" "syntactic-zest" "semantic-noguidance" "syntactic-noguidance"; do
+for prefix in "semantic-zest"; do
+    for ((i=20; i<=39; i++)); do
+        old_name="../expfinal3/nextflow-${prefix}-${i}"
+        new_number=$((i-20))
+        new_name="../expfinal3/nextflow-${prefix}-${new_number}"
         
         if [ -d "$old_name" ]; then
             echo "copyinging $old_name to $new_name "
             
-	    cp -r "$old_name" "$new_name"
+	        mv "$old_name" "$new_name"
         fi
     done
 done
