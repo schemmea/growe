a2159b444ab6b7bc1a1969690d17f7b10f9eb60d
Alena <alena.sch95@gmail.com>
2023-09-27 22:13:02 +0200

    move experiments to other repo

--- scripts/data-processing/download_data_corpus_afl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#remotepath="/home/alena/source/ma_test2/exp1/"
-remotepath="/home/alena/source/ma_test2/exp2"
-localpath="$HOME/fulldownload"
-
-scp -r alena@213.202.208.247:${remotepath} "${localpath}/exp2"
-
-
--- scripts/data-processing/download_data_zip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#remotepath="/home/alena/source/ma_test2/exp1/"
-remotepath="/home/alena/source/growe/expfinal3"
-localpath="$HOME/fulldownload"
-
-scp -r alena@213.202.208.247:${remotepath} "${localpath}/expfinal3"
-
-
